0859732b6dbf32e2a8a204faced28189d9e54bac
Airenas Vaičiūnas <airenass@gmail.com>
2022-01-04 10:40:00 +0200

    Add validate HTTP response helper function

--- pkg/goapp/utils.go
+++ pkg/goapp/utils.go
@@ -1,6 +1,9 @@
 package goapp
 
 import (
+	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 	"time"
 )
@@ -25,4 +28,30 @@ func Estimate(name string) func() {
 	return func() {
 		Log.Infof("%s took %v", name, time.Since(start))
 	}
+}
+
+//ValidateHTTPResp returns error if code is not in [200, 299]
+// bodyLen - size of bytes to try read body
+func ValidateHTTPResp(resp *http.Response, bodyLen int) error {
+	if !(resp.StatusCode >= 200 && resp.StatusCode <= 299) {
+		return fmt.Errorf("resp code: %d%s", resp.StatusCode, getBodyStr(resp.Body, bodyLen))
+	}
+	return nil
+}
+
+func getBodyStr(rd io.Reader, l int) string {
+	if l > 0 {
+		bytes := make([]byte, l+1)
+		n, err := rd.Read(bytes)
+		if err != nil && err != io.EOF {
+			Log.Warn(err)
+		}
+		if n > l {
+			return "\n" + string(bytes[:l]) + "..."
+		}
+		if n > 0 {
+			return "\n" + string(bytes[:n])
+		}
+	}
+	return ""
 }
--- pkg/goapp/utils_test.go
+++ pkg/goapp/utils_test.go
@@ -1,6 +1,10 @@
 package goapp
 
 import (
+	"bytes"
+	"io"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +13,59 @@ import (
 func TestHidePass_Mongo(t *testing.T) {
 	assert.Equal(t, "mongodb://mongo:27017", HidePass("mongodb://mongo:27017"))
 	assert.Equal(t, "mongodb://l:----@mongo:27017", HidePass("mongodb://l:olia@mongo:27017"))
+}
+
+func TestValidateHTTPResp(t *testing.T) {
+	tests := []struct {
+		name       string
+		code       int
+		body       string
+		l          int
+		wantErrStr string
+	}{
+		{name: "200", code: 200, body: "OK", l: 100, wantErrStr: ""},
+		{name: "299", code: 299, body: "OK", wantErrStr: ""},
+		{name: "400", code: 400, body: "error", l: 100, wantErrStr: "resp code: 400\nerror"},
+		{name: "503", code: 503, body: "error", l: 100, wantErrStr: "resp code: 503\nerror"},
+		{name: "503 no err", code: 503, body: "error", l: 0, wantErrStr: "resp code: 503"},
+		{name: "400 long", code: 400, body: strings.Repeat("error", 50), l: 100, wantErrStr: "resp code: 400\n" +
+			strings.Repeat("error", 50)[:100] + "..."},
+		{name: "400 long", code: 400, body: strings.Repeat("error", 50)[:100], l: 100, wantErrStr: "resp code: 400\n" +
+			strings.Repeat("error", 50)[:100]},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tResp := httptest.NewRecorder()
+			tResp.Body = bytes.NewBuffer([]byte(tt.body))
+			tResp.Code = tt.code
+			err := ValidateHTTPResp(tResp.Result(), tt.l)
+			if tt.wantErrStr != "" {
+				assert.Equal(t, tt.wantErrStr, err.Error())
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
+
+func Test_getBodyStr(t *testing.T) {
+	tests := []struct {
+		name string
+		rd io.Reader
+		l  int
+		want string
+	}{
+		{name:"Empty", rd: strings.NewReader(""), l: 10, want: ""},
+		{name:"New line", rd: strings.NewReader("a"), l: 10, want: "\na"},
+		{name:"Trim", rd: strings.NewReader(strings.Repeat("a", 20)), l: 10, want: "\n" + strings.Repeat("a", 10) + "..."},
+		{name:"Full", rd: strings.NewReader(strings.Repeat("a", 20)), l: 20, want: "\n" + strings.Repeat("a", 20)},
+		{name:"Long", rd: strings.NewReader(strings.Repeat("a", 20000)), l: 20000, want: "\n" + strings.Repeat("a", 20000)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getBodyStr(tt.rd, tt.l); got != tt.want {
+				t.Errorf("getBodyStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
