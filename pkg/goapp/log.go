624be549c4c454e7c31356679e3c35b84a2d9e26
Airenas Vaičiūnas <airenass@gmail.com>
2022-12-19 08:48:50 +0200

    Fix log to json

--- pkg/goapp/log.go
+++ pkg/goapp/log.go
@@ -19,7 +19,7 @@ func initLog() {
 	if err != nil {
 		Log.Error().Err(err).Msgf("can't parse %s", sl)
 	} else {
-		Log = log.Logger.Level(l).Output(zerolog.NewConsoleWriter())
+		Log = log.Logger.Level(l)
 	}
 	if strings.ToLower(Config.GetString("logger.out")) == "console" {
 		Log = log.Logger.Level(l).Output(zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) { w.TimeFormat = "2006-01-02T15:04:05.000" }))
